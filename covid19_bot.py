36f5da6e602c8f06c5367b0f4e5663e7f3ddffb3
vikramborana <vikramborana9603@gmail.com>
2020-07-05 23:30:29 +0530

    Initial commit

---
+++ covid19_bot.py
@@ -0,0 +1,82 @@
+import tweepy
+import requests
+import json
+import locale
+import time
+
+
+
+def create_tweet():
+	locale.setlocale(locale.LC_ALL,'en_IN.utf8')
+	response = requests.get('https://api.covid19india.org/data.json')
+	data = response.json()
+	
+	actual_data=data["statewise"][0] 
+	
+	deltaconfirmed = int(actual_data["deltaconfirmed"])
+	deltarecovered = int(actual_data["deltarecovered"])
+	deltadeaths = int(actual_data["deltadeaths"])
+	time = actual_data["lastupdatedtime"]
+	totconfirmed = int(actual_data["confirmed"])
+	totrecovered = int(actual_data["recovered"])
+	totdeaths = int(actual_data["deaths"])
+	
+	tweet = f"""	Covid19 Updates(INDIA)
+	
+Last updated on : {time}
+
+Todays Report:
+New Cases : {locale.format_string("%d",deltaconfirmed,grouping=True)}
+New Recovery: {locale.format_string("%d",deltarecovered,grouping=True)}
+New Deaths: {locale.format_string("%d",deltadeaths,grouping=True)}
+
+Overall:	
+Total : {locale.format_string("%d",totconfirmed,grouping=True)}
+Recovered : {locale.format_string("%d",totrecovered,grouping=True)}
+Deaths : {locale.format_string("%d",totdeaths,grouping=True)}	   
+	
+"""
+	return tweet
+
+
+	
+
+
+def tweeting():
+	CONSUMER_KEY =''   #Add ur own keys
+	CONSUMER_SECRET =''
+	ACCESS_KEY = ''
+	ACCESS_SECRET = ''
+
+
+	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+	api = tweepy.API(auth)
+	
+	try:
+		api.verify_credentials()
+		print('Authentication Successful')
+	except:
+		print('Error while authenticating API')
+		sys.exit(1)
+	tweet = create_tweet()
+	try:
+		api.update_status(tweet)
+		print("Tweet Successful")
+	except tweepy.TweepError as error:
+		if error.api_code == 187:
+			print("Duplicate message")
+		else:
+			raise error			
+     	
+while True:
+	tweeting() 	
+	time.sleep(10800)
+	       
+
+
+
+
+
+
+	
